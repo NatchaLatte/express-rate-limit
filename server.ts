015a6cbc70bfc55c519dfbab65cbc9051a3917d4
NatchaLatte <foxteary@gmail.com>
2024-11-08 23:51:44 +0700

    first commit

---
+++ server.ts
@@ -0,0 +1,23 @@
+import express, { Express, Request, Response } from "express";
+import { rateLimit } from 'express-rate-limit'
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 4000;
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+app.use(limiter);
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
